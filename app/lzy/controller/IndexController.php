01874d0c0d05356735381c0c46230e85975fabce
lzy <lzy_email@sina.cn>
2019-01-22 22:36:46 +0800

    修改忽略文件

---
+++ app/lzy/controller/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2018 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 罗志禹 <lzy_email@sian.cn>
+// +----------------------------------------------------------------------
+namespace app\lzy\controller;
+
+use cmf\controller\HomeBaseController;
+
+class IndexController extends HomeBaseController
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+    public function addTest(){
+        return view();
+    }
+}
